f3dfad6c99068de624b3ddfdcdf51155839f21d8
Trymad <dummymail@mail.com>
2024-09-08 23:03:02 +0300

    add move method for king (hardcore need refactor)

--- src/js/model/piece/King.ts
+++ src/js/model/piece/King.ts
@@ -5,12 +5,31 @@ import { Cell } from "../board/Cell.js";
 import { Piece, PieceType } from "./Piece.js";
 
 export class King extends Piece {
-  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
-    throw new Error("Method not implemented.");
-  }
   
   constructor(color: Color) {
     super(color, PieceType.KING);
   }
+
+  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
+    const currentPoint: Point = this.getCell()!.getPoint();
+    const pointArr: Point[] = []
+
+    pointArr.push(new Point(currentPoint.getX() - 1, currentPoint.getY() - 1));
+    pointArr.push(new Point(currentPoint.getX() + 1, currentPoint.getY() + 1));
+    pointArr.push(new Point(currentPoint.getX() - 1, currentPoint.getY() + 1));
+    pointArr.push(new Point(currentPoint.getX() + 1, currentPoint.getY() - 1));
+
+    pointArr.push(new Point(currentPoint.getX() + 1, currentPoint.getY() - 0));
+    pointArr.push(new Point(currentPoint.getX() - 1, currentPoint.getY() - 0));
+    pointArr.push(new Point(currentPoint.getX() + 0, currentPoint.getY() - 1));
+    pointArr.push(new Point(currentPoint.getX() + 0, currentPoint.getY() + 1));
+
+    
+    return pointArr
+    .filter(board.inBorder, board)
+    .map(board.getCell, board)
+    .filter(cell => 
+      (!cell.isEmpty() && cell.getPiece()!.getColor() != this.getColor()) || cell.isEmpty())
+    }
 
 }
