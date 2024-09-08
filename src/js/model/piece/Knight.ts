685c125ce466ffa647478c7a908e16c71dec06a4
Trymad <dummymail@mail.com>
2024-09-08 22:54:10 +0300

    add move method for knight (need refactor hardcode)

--- src/js/model/piece/Knight.ts
+++ src/js/model/piece/Knight.ts
@@ -1,14 +1,33 @@
 import { Color } from "../Color.js";
+import { Point } from "../Point.js";
 import { Board } from "../board/Board.js";
 import { Cell } from "../board/Cell.js";
 import { Piece, PieceType } from "./Piece.js";
 
 export class Knight extends Piece {
-  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
-    throw new Error("Method not implemented.");
-  }
   
   constructor(color: Color) {
     super(color, PieceType.KNIGHT);
   }
+
+  //fix cringe hardcode
+  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
+    const currentPoint: Point = this.getCell()!.getPoint();
+    const pointArr: Point[] = []
+    pointArr.push(new Point(currentPoint.getX() - 2, currentPoint.getY() - 1));
+    pointArr.push(new Point(currentPoint.getX() - 1, currentPoint.getY() - 2));
+    pointArr.push(new Point(currentPoint.getX() - 2, currentPoint.getY() + 1));
+    pointArr.push(new Point(currentPoint.getX() - 1, currentPoint.getY() + 2));
+
+    pointArr.push(new Point(currentPoint.getX() + 2, currentPoint.getY() - 1));
+    pointArr.push(new Point(currentPoint.getX() + 1, currentPoint.getY() - 2));
+    pointArr.push(new Point(currentPoint.getX() + 2, currentPoint.getY() + 1));
+    pointArr.push(new Point(currentPoint.getX() + 1, currentPoint.getY() + 2));
+    
+    return pointArr
+    .filter(board.inBorder, board)
+    .map(board.getCell, board)
+    .filter(cell => 
+      (!cell.isEmpty() && cell.getPiece()!.getColor() != this.getColor()) || cell.isEmpty())
+    }
 }
