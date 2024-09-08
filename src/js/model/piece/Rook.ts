f495697a8b33f50a02253175a627e2683c18e868
Trymad <dummymail@mail.com>
2024-09-08 22:14:04 +0300

    add rock moving method

--- src/js/model/piece/Rook.ts
+++ src/js/model/piece/Rook.ts
@@ -1,14 +1,40 @@
 import { Color } from "../Color.js";
+import { Point } from "../Point.js";
 import { Board } from "../board/Board.js";
 import { Cell } from "../board/Cell.js";
 import { Piece, PieceType } from "./Piece.js";
 
 export class Rook extends Piece {
-  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
-    throw new Error("Method not implemented.");
-  }
   
   constructor(color: Color) {
     super(color, PieceType.ROOK);
+  }
+  
+  public getAvailableCellToMove(board: Board, pieceCell: Cell): Cell[] {
+    return [...this.getAvailablePointInDirection(0, 1, board),
+      ...this.getAvailablePointInDirection(0, -1, board),
+      ...this.getAvailablePointInDirection(1, 0, board),
+      ...this.getAvailablePointInDirection(-1, 0, board)
+    ];  
+  }
+
+  private getAvailablePointInDirection(xchange: number, ychange: number, board: Board): Cell[] {
+    const cellArr: Cell[] = [];
+    const point: Point = this.getCell()!.getPoint();
+    point.addToX(xchange);
+    point.addToY(ychange);
+
+    while(board.inBorder(point)) {
+      const currentCell = board.getCell(point);
+      cellArr.push(currentCell);
+      if(!currentCell.isEmpty()) {
+        if(currentCell.getPiece()?.getColor() == this.getColor()) cellArr.pop();
+        break;
+      }
+      point.addToX(xchange);
+      point.addToY(ychange);
+    }
+
+    return cellArr;
   }
 }
